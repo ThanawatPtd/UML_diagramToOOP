230b60718cfc963ed53501cacdb92d55f732dad5
Thanawat Potidet <thanawatpotidet@Thanawats-MacBook-Pro.local>
2024-06-26 15:23:36 +0700

    check players more than 8 player and delete count function

--- src/MGame.java
+++ src/MGame.java
@@ -17,9 +17,10 @@ public class MGame {
         dice.add(new Die());
         this.board = new Board();
         this.players = playerList;
-        if (this.players.size() < 2) {
-            throw new IllegalArgumentException("There must be at least 2 players to start the game.");
+        if (this.players.size() < 2 || this.players.size() > 8) {
+            throw new IllegalArgumentException("The number of players must be between 2 and 8.");
         }
+
     }
 
     public void playGame(){
@@ -33,14 +34,6 @@ public class MGame {
     }
 
     private void playRound(int roundCount){
-        players.get(roundCount % count()).takeTurn();
-    }
-
-    public int count(){
-        int count = 0;
-        for(Player player : players){
-            count++;
-        }
-        return count;
+        players.get(roundCount % players.size()).takeTurn();
     }
 }
